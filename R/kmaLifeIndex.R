711c83adc73a47a3fa33708cd04268db75319d60
hss0116 <hss0116@macrogen.com>
2019-05-17 15:32:51 +0900

    error code correction.

--- R/kmaLifeIndex.R
+++ R/kmaLifeIndex.R
@@ -235,8 +235,14 @@ kmaLifeIndex <- function(key, time = seq(0, 21, 3), localeCode = NULL, localeNam
   ### 4. merge data by index type.
   data <- list(); length(data) <- length(type)
   for(i in 1:length(data)){
-    data[[i]] <- bind_rows(all.data[lapply(all.data, function(x)
-      x$type == datagokR::kma_lifeIndex_urlType[type][i]) %>% unlist %>% which]) %>% as.tbl %>%
+    tmp.d <- bind_rows(all.data[lapply(all.data, function(x)
+      x$type == datagokR::kma_lifeIndex_urlType[type][i]) %>% unlist %>% which]) %>% as.tbl
+
+    if(nrow(tmp.d) == 0){
+      next
+    }
+
+    data[[i]] <- tmp.d %>%
       mutate("time" = strptime(.data$time, format='%Y%m%d%H') %>% as.character,
              "locale" = as.numeric(.data$locale))
 

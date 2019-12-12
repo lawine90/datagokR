76122342e535d8f22d3ac61588b08351080ecd3c
lawine90 <power1sky@gmail.com>
2019-12-12 16:55:48 +0900

    new function: find_xml

---
+++ R/find_xml.R
@@ -0,0 +1,14 @@
+#' Find text from xml2::read_xml object and if not exist, return NA.
+#'
+#' @param xml result of xml2::read_xml function.
+#' @param tag tag which you want to find.
+#'
+find_xml <- function(xml, tag){
+  return(
+    ifelse(
+      length(xml2::xml_text(xml2::xml_find_all(xml, tag))) != 0,
+      xml2::xml_text(xml2::xml_find_all(xml, tag)),
+      NA
+    )
+  )
+}

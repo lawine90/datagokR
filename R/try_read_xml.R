59d3f85453706572a17881349f7f428b6d0e04d4
lawine90 <power1sky@gmail.com>
2019-12-19 13:29:51 +0900

    assist function: try_read_xml

---
+++ R/try_read_xml.R
@@ -0,0 +1,16 @@
+#' Try xml2::read_xml function at least 5 times If got errors, save error message.
+#'
+#' @param url url want to read.
+#' @param times trying times.
+#'
+try_read_xml <- function(url, times = 5){
+  ii <- 0
+  repeat{
+    ii <- ii + 1
+    xml <- tryCatch(
+      {xml2::read_xml(url, encoding = 'UTF-8')},
+      error = function(e){e}
+    )
+    if(!any(class(xml) == 'error') | ii == times) break
+  }; return(xml)
+}

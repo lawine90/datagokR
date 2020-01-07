e47523d8ee59750e84441cffaa18a2b4519197ec
lawine90 <power1sky@gmail.com>
2020-01-07 15:42:34 +0900

    update internal function

--- R/find_xml.R
+++ R/find_xml.R
@@ -3,10 +3,16 @@
 #' @param xml result of xml2::read_xml function.
 #' @param tag tag which you want to find.
 #'
-find_xml <- function(xml, tag){
-  if(length(xml2::xml_text(xml2::xml_find_all(xml, tag))) != 0){
-    return(xml2::xml_text(xml2::xml_find_all(xml, tag)))
-  }else{
-    return(NA)
+find_xml <- function(xml, tag, type = 'char'){
+  if(class(xml) != 'xml_nodeset'){
+    stop('Input xml should be "xml_nodeset" class')
+  }
+  if(!grepl('./', tag)){
+    warning('Input tag should be start with "./****"')
+  }
+  if(type == 'char'){
+    return(xml2::xml_text(xml2::xml_find_first(xml, tag)))
+  }else if(type == 'num'){
+    return(as.numeric(xml2::xml_text(xml2::xml_find_first(xml, tag))))
   }
 }
